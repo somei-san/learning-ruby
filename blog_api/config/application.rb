345541994e4774252072c2ba14814013a237aa5f
Somei Tanoue <113166576+somei-san@users.noreply.github.com>
2026-05-23 14:37:05 +0900

    🔧 RSpec / factory_bot_rails を導入

--- blog_api/config/application.rb
+++ blog_api/config/application.rb
@@ -28,5 +28,10 @@ module BlogApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.test_framework :rspec
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
   end
 end
